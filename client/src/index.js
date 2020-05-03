855a4b76ac1cbac4deb5982bef90784e613b5941
Victor Wang <victor.wang@open.edu.au>
2020-05-03 17:57:23 +1000

    remove devDependencies

--- client/src/index.js
+++ client/src/index.js
@@ -6,7 +6,7 @@ import BrandBand from '@salesforce/design-system-react/module/components/brand-b
 import App from './components/App/App';
 import { GlobalProvider } from './hooks/globalHook';
 //use this 3rd party pkg to connect useReducer Hook and Redux Dev Tool
-import { StateInspector } from 'reinspect';
+// import { StateInspector } from 'reinspect';
 
 ReactDOM.render(
   <IconSettings iconPath="../public/assets/icons">
@@ -15,11 +15,11 @@ ReactDOM.render(
       theme="lightning-blue"
       size="small"
     >
-      <StateInspector>
-        <GlobalProvider>
-          <App />
-        </GlobalProvider>
-      </StateInspector>
+      {/* <StateInspector> */}
+      <GlobalProvider>
+        <App />
+      </GlobalProvider>
+      {/* </StateInspector> */}
     </BrandBand>
   </IconSettings>,
   document.getElementById('root')
